87f8990db5bc98cd25c249518885a48f9df32540
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-21 13:39:46 +0100

    Update font size in NameCard component

--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -1,7 +1,7 @@
 export default function Namecard() {
   return (
     <div className='bg-lime-400 text-black w-full h-full flex justify-center items-center flex-col'>
-      <h1 className='text-6xl font-bold uppercase tracking-wide'>
+      <h1 className='text-5xl font-bold uppercase tracking-wide'>
         Evgeniy Pimenov
       </h1>
       <h2 className='text-2xl font-bold uppercase tracking-widest'>
