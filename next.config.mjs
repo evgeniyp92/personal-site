7f9894bd01468227be61a5597ae8b7bd25d65bae
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-02-22 12:24:25 +0100

    install next/mdx and do basic implementation
    problem: compiler isnt happy im using require

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+const withMDX = require('@next/mdx')(); // immediately invoking an import to get the returned function?
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+};
 
 export default nextConfig;
