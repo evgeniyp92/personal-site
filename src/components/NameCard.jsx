609cbd39d7f0805b8d51b460ae78c5609e47b4fa
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-02-22 13:13:04 +0100

    update classes on landing page for mobile

--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -1,10 +1,10 @@
 export default function Namecard() {
   return (
     <div className='bg-lime-400 text-black w-full h-full flex justify-center items-center flex-col'>
-      <h1 className='text-5xl font-bold uppercase tracking-wide'>
+      <h1 className='text-xl text-center lg:text-5xl lg:rotate-0 font-bold uppercase tracking-wide'>
         Evgeniy Pimenov
       </h1>
-      <h2 className='text-2xl font-bold uppercase tracking-widest'>
+      <h2 className='text-sm lg:text-2xl lg:rotate-0 lg:font-bold tracking-widest text-center'>
         Full-Stack Developer
       </h2>
     </div>
