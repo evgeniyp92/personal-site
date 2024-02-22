d3a33bbcbea5e29f8f06781e1cf4fda0135de239
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-02-22 12:11:55 +0100

    Implement Markdown rendering w/ react-markdown

--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -1,10 +1,14 @@
 // get route params
+import Markdown from 'react-markdown';
+
+const markdown = '# Hello world';
 
 export default function ProjectPage({ params }) {
   return (
     <div>
       ProjectPage
       <h1>Slug: {params.slug}</h1>
+      <Markdown>{markdown}</Markdown>
     </div>
   );
 }
