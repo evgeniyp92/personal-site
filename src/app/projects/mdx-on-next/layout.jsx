acdefed85f7376a9145f9a896b793000f5ac4b77
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-02-23 11:01:18 +0100

    fix a css bug causing bad scaling on mobile for markdown

--- src/app/projects/mdx-on-next/layout.jsx
+++ src/app/projects/mdx-on-next/layout.jsx
@@ -1,7 +1,7 @@
 export default function MDXLayout({ children }) {
   return (
-    <div className='container mx-auto flex justify-center'>
-      <article className='prose prose-base lg:prose-xl prose-pre:p-0'>
+    <div className='container mx-auto'>
+      <article className='prose prose-sm lg:prose-base prose-pre:p-0'>
         {children}
       </article>
     </div>
